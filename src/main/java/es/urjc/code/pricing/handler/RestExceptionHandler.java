26baa17a3510094f9cfe3817a5cb8007234c9a16
François <fpoirier2002@gmail.com>
2020-11-02 17:57:14 +0100

    code smell

--- src/main/java/es/urjc/code/pricing/handler/RestExceptionHandler.java
+++ src/main/java/es/urjc/code/pricing/handler/RestExceptionHandler.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
